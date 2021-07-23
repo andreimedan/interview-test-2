88cbfc9beb6753ab3d61336d45449dcb622db1f3
andreimedan <andrei.medan@yahoo.com>
2021-07-23 15:46:00 +0300

    fix_junit_test

--- src/main/java/eu/cec/digit/comref/interview/persistent/repository/InternetServiceProviderRepository.java
+++ src/main/java/eu/cec/digit/comref/interview/persistent/repository/InternetServiceProviderRepository.java
@@ -1,5 +1,7 @@
 package eu.cec.digit.comref.interview.persistent.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import eu.cec.digit.comref.interview.persistent.domain.InternetServiceProvider;
@@ -9,4 +11,6 @@ import java.util.List;
 public interface InternetServiceProviderRepository extends JpaRepository<InternetServiceProvider, String> {
 
     List<InternetServiceProvider> findByAvailable(boolean available);
+    Page<InternetServiceProvider> findByAvailable(Pageable page, boolean available);
+    Page<InternetServiceProvider> findAll(Pageable pageable);
 }
--- src/main/java/eu/cec/digit/comref/interview/persistent/repository/TownRepository.java
+++ src/main/java/eu/cec/digit/comref/interview/persistent/repository/TownRepository.java
@@ -1,9 +1,12 @@
 package eu.cec.digit.comref.interview.persistent.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import eu.cec.digit.comref.interview.persistent.domain.Town;
 
 public interface TownRepository extends JpaRepository<Town, String> {
-
+    Page<Town> findAll(Pageable pageable);
+    Page<Town> findByInhabitants(Pageable page, Integer inhabitants);
 }
